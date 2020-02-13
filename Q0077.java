c29c9ba86a525baeab2f9917274566a351eba3c3
Yifei Feng <yifei.feng@hotmail.com>
2020-02-12 21:27:30 -0500

    Create Q0077.java

---
+++ Q0077.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    private List<List<Integer>> result = new ArrayList<List<Integer>>();
+    
+    private int endNumber; 
+    
+    private int limit;
+        
+    public List<List<Integer>> combine(int n, int k) {
+        endNumber = n;
+        limit = k;
+        if(n > 0 && k > 0){
+            doCombination(1, new ArrayList<Integer>());
+        }
+        return result;
+    }
+    
+    private void doCombination(int currentStart, List<Integer> previousList){
+        if(previousList.size() == limit){
+            result.add(previousList);
+            return;
+        }
+        
+        if(currentStart > endNumber){
+            return;
+        }
+        
+        for(int i = currentStart; i <= endNumber; i++){
+            List<Integer> currentList = new ArrayList<Integer>(previousList);
+            currentList.add(i);
+            doCombination(i + 1, currentList);
+        }
+    }
+    
+}
